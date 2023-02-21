36c3240aa1fc9ad68e3d38dabb6ccafa47072282
lm101845 <1018457683@qq.com>
2023-02-21 18:16:33 +0800

    第126-128集看完

---
+++ 03-鹏哥C语言/02-代码手敲/知识进阶/22-指针作业讲解.c
@@ -0,0 +1,14 @@
+//2023Äê2ÔÂ21ÈÕ13:46:23
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+	//int (*  (*F)(int, int))(int);
+	//int(*  (*F)(int, int) );
+	system("pause");
+	return EXIT_SUCCESS;
+}
