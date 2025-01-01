113b7dfac6888a630563ccc0cd595c447a7cba7d
liming <1018457683@qq.com>
2025-01-01 09:13:05 +0800

    提交

--- 03-鹏哥C语言/01-课程资料/上课代码-2021版C语言-【比特就业课提供】/test_4_2/test_4_2/global.c
+++ 03-鹏哥C语言/01-课程资料/上课代码-2021版C语言-【比特就业课提供】/test_4_2/test_4_2/global.c
@@ -1,4 +1,11 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+{\rtf1\ansi\ansicpg1252\paperw11900\paperh16840\margl0\margr0\margt1440\margb1440\vieww18540\viewh17280\viewscale100\lin0\rin0
+{\papercolor16777215}
+{\fonttbl\f0\fnil\fcharset0 Helvetica;}
+{\colortbl\red0\green0\blue0;\red255\green255\blue255;}
+\pard
+{\*\pageHeader Helvetica::12px::right::black::0.5::NO::Header Author: Zoe Appleseed [date] - Page [pagenumber] of [totalpages]}
+{\*\pageFooter Helvetica::12px::right::black::0.5::NO::Footer Author: Zoe Appleseed [title] - Page [pagenumber] of [totalpages]}
+{\*\background {\shp{\*\shpinst\shpleft0\shptop0\shpright0\shpbottom0\shpfhdr0\shpbxmargin\shpbymargin\shpwr0\shpwrk0\shpfblwtxt1\shpz0\shplid1025{\sp{\sn shapeType}{\sv 1}}{\sp{\sn fFlipH}{\sv 0}}{\sp{\sn fFlipV}{\sv 0}}{\sp{\sn fillColor}{\sv 16777215}}{\sp{\sn fFilled}{\sv 1}}{\sp{\sn lineWidth}{\sv 0}}{\sp{\sn fLine}{\sv 0}}{\sp{\sn bWMode}{\sv 9}}{\sp{\sn fBackground}{\sv 1}}}}}
 
-
-int g_val = 2022;
+\f0\fs24 \cf0 #define _CRT_SECURE_NO_WARNINGS 1int g_val = 2022;\
+}
